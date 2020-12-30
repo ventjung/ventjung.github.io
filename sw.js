3af8fe513026c940cef61f775a40d74476b43785
ventjung <43715904+ventjung@users.noreply.github.com>
2020-12-30 11:21:38 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-console.log('ok');
+self.addEventListener('install', function(event) {
+  console.log('install ok');
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('fetch ok');
+});
